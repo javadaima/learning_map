75d65257e28adab0353825b41d3295814e498b7d
chenzhigang <1549318019@qq.com>
2020-04-18 04:06:31 +0800

    测试jenkins

--- hello_world/src/main/java/com/example/hello_world/controller/TestController.java
+++ hello_world/src/main/java/com/example/hello_world/controller/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping("/hello")
     public String hello(){
-        return "hello world1992";
+        return "hello world199202";
     }
 }
