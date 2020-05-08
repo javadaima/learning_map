442cd94c88db353a5f5c3e91a26b72653ac787b5
chenzhigang <1549318019@qq.com>
2020-05-09 00:37:51 +0800

    测试jenkins

--- hello_world/src/main/java/com/example/demo/HelloWorldApplication.java
+++ hello_world/src/main/java/com/example/demo/HelloWorldApplication.java
@@ -1,4 +1,4 @@
-package com.example.hello_world;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- hello_world/src/main/java/com/example/demo/controller/TestController.java
+++ hello_world/src/main/java/com/example/demo/controller/TestController.java
@@ -1,18 +1,15 @@
-package com.example.hello_world.controller;
+package com.example.demo.controller;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class TestController {
     @RequestMapping("/hello")
     public String hello(){
-        return "hello world";
+        return "hello world111";
     }
-
-
-
-
 
 }
